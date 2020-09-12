80f4d52e63340ad28ab0d0134c9db6555c11137e
umar567 <umarmuzammil111@gmail.com>
2020-09-12 16:58:09 +0500

    Goods In Transit: 1- Add custom button: -Get items from purchase order -Create purchase receipt 2- Also add ther functionality

--- shipments/shipments/doctype/goods_in_transit_note/goods_in_transit_note.js
+++ shipments/shipments/doctype/goods_in_transit_note/goods_in_transit_note.js
@@ -2,6 +2,76 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Goods In Transit Note', {
+
+	refresh: function(frm) {
+			
+		if (frm.is_new()){
+			frm.add_custom_button(__('Get items from purchase order'), () => frm.trigger('get_items_from_purchase_order'), __('Get Items'));
+			
+		}
+		if (cur_frm.doc.docstatus == 1){
+		frm.add_custom_button('Create purchase receipt', () => {
+			let doc = frm.doc
+			frappe.run_serially([
+				() => frappe.new_doc('Purchase Receipt'),
+				() => {
+					cur_frm.doc.supplier = doc.invoiced_by;
+					for (let i in doc.items){
+						cur_frm.doc.items = []
+						cur_frm.add_child('items', {
+							item_code : doc.items[i].item_code,
+							received_qty : doc.items[i].qty,
+							uom : doc.items[i].uom,
+							rate : doc.items[i].rate,
+							amount : doc.items[i].total_amount,
+							qty : doc.items[i].qty
+						})
+					}
+					cur_frm.refresh_field('items')
+				}
+			]);
+		});
+	}
+
+	
+		
+	},
+
+	get_items_from_purchase_order: function (frm) {
+		if (!frm.doc.invoiced_by){
+			frappe.msgprint('Supplier not selected')
+		}
+		else{
+		new frappe.ui.form.MultiSelectDialog({
+			doctype: "Purchase Order",
+			target: cur_frm,
+			setters: {
+				company: cur_frm.doc.company,
+				
+			},
+			get_query() {
+				return {
+					filters: { 
+						docstatus: ['=', 1],
+						supplier: cur_frm.doc.invoiced_by
+					}
+				}
+			},
+			action(selections) {
+				console.log(selections[0])
+				if (selections.length == 0){
+					frappe.msgprint('Select a Purchase Order')
+				}
+				else{
+					cur_frm.doc.items = null
+					frm.call('get_items_from_purchase_order',selections)
+					cur_frm.refresh();
+				}
+			}
+		});
+	}
+	},
+
 	calculate_amount: function (frm){
 		// triggers when you change row value in Goods In Transit Note Item
 		let doc = frm.doc;
--- shipments/shipments/doctype/goods_in_transit_note/goods_in_transit_note.py
+++ shipments/shipments/doctype/goods_in_transit_note/goods_in_transit_note.py
@@ -3,8 +3,34 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+import datetime
 
 class GoodsInTransitNote(Document):
-	pass
+	def get_items_from_purchase_order(self,selections):
+		
+		total_qty = 0
+		amount = 0
+		self.items = []
+		for i in selections:
+			purchase_order = frappe.get_doc('Purchase Order', i)
+			for item in purchase_order.items:
+				self.append('items',{
+					'item_code' : item.item_code,
+					'qty': item.qty,
+					'rate': item.rate,
+					'total_amount' : item.amount	
+				}
+				)
+				total_qty += item.qty
+				amount += item.amount
+		self.total_qty = total_qty
+		self.amount = amount
+				
+		
+		
+
+		 
+		
+	
